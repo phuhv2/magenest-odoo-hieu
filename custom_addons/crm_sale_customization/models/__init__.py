809edc47cf32a91510f4a2c3950275101aac8e83
hieunqdev <hieunqdev@gmail.com>
2022-11-22 11:59:11 +0700

    ODOO_RM_INTERN-088: add gitognore

--- custom_addons/crm_sale_customization/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import plan_sale_order
-from . import advanced_crm_quotations
--- custom_addons/hn_crm_sales/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from . import plan_sale_order
-from . import quotation
